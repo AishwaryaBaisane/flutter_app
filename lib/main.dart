9d63044663b8c2dfdbe9a6a20862d50b3b0845a5
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2024-03-18 15:26:48 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,14 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -45,11 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+  
       _counter++;
     });
   }
@@ -65,20 +53,12 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: true,
         backgroundColor: Colors.red,
         actions: [
-               // Icon(Icons.star,color:Colors.white,),
+             
                Icon(Icons.more_vert,color:Colors.white,)],
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        // backgroundColor: return return Theme.of(context).colorScheme,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-
-
+  
       ),
       body: const Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+       
         child: Column(
 
           mainAxisAlignment: MainAxisAlignment.center,
@@ -93,18 +73,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
                ),
             ),
-            // Text(
-            //   // '$_counter',
-            //   // style: Theme.of(context).textTheme.headlineMedium,
-            // ),
+         
           ],
         ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
+   
     );
   }
 }
